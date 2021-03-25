af87f808b6d940958173397499f03db896cae846
Richard Guaman <gs.richarda@gmail.com>
2021-03-25 11:52:43 +0000

    Post endpoint has been updated - works with ajx format

--- src/views.py
+++ src/views.py
@@ -21,11 +21,12 @@ def profile(request, *args, **kwargs):
 def create_post_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
-    print("next_url", next_url)
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form related logic
         obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201)  # 201 == created items
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
@@ -34,8 +35,7 @@ def create_post_view(request, *args, **kwargs):
 
 def tweets_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content,
-                    "likes": random.randint(0, 122)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
