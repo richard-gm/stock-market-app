7a7fdd54baf29febb74f4e26ed62c56acced8929
Richard Guaman <gs.richarda@gmail.com>
2021-04-10 00:31:23 +0100

    List of tweets can now be displayed on the front end (manually)

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -2,12 +2,35 @@ import React, {useEffect, useState} from "react"; // run HTTP request from the b
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function loadTweets(callback) {
+    const xhr = new XMLHttpRequest() // requesting data as HTTP
+    const method = 'GET' // "POST"
+    const url = "http://127.0.0.1:8000/profile/api/tweets/"   // Goes to this url
+    const responseType = "json"
+    xhr.responseType = responseType
+    xhr.open(method, url)
+    xhr.onload = function() {
+        callback(xhr.response, xhr.status)
+    }
+    xhr.onerror = function (e) {
+        console.log(e)
+        callback({"message": "The request was an error"}, 400)
+    }
+  xhr.send()
+}
 
+function App() {
   const [tweets, setTweets] = useState([])
-  useEffect(() => { // gets data from the backend (now running on the front end during implementation)
-    const tweetItems = [{"content": 123},{"content": 321}]
-    setTweets(tweetItems)
+  useEffect(() => {
+    const myCallback = (response, status) => {
+      console.log(response, status)
+      if (status === 200){
+        setTweets(response)
+      } else {
+        alert("There was an error")
+      }
+    }
+    loadTweets(myCallback)
   }, [])
   return (
     <div className="App">
