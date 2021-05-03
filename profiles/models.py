4582640745ee231ac99f772ae6db3a3f0867a3cb
Richard Guaman <gs.richarda@gmail.com>
2021-05-03 16:25:05 +0100

    Updating user's profile completed

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
 
 # Create your models here.
 User = settings.AUTH_USER_MODEL
@@ -9,6 +10,14 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # deletes the user and all its data
     location = models.CharField(max_length=220, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
+
+
+def user_did_save(sender, instance, created, *args, **kwargs):
+    if created:
+        Profile.objects.get_or_create(user=instance)
+
+
+post_save.connect(user_did_save, sender=User)
 
 
 class Stocks(models.Model):
