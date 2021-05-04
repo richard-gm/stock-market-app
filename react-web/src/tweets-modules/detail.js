1a56346cbbab1f7cfdd55641266da8a6ec55ab5c
Richard Guaman <gs.richarda@gmail.com>
2021-05-04 21:58:01 +0100

    pagination in react handled

--- react-web/src/tweets-modules/detail.js
+++ react-web/src/tweets-modules/detail.js
@@ -3,18 +3,14 @@ import {ActionBtn} from "./buttons";
 
 export function ParentTweet(props){
     const {tweet} = props
-    return tweet.parent ? <div className='row'>
-        <div className='col-11 mx-auto p-3 border rounded'>
-            <p className='mb-0 text-muted small'>Retweet</p>
-            <Tweet hideActions className={' '} tweet={tweet.parent} />
-        </div>
-    </div> : null
+    return tweet.parent ? <Tweet isRetweet retweeter={props.retweeter} hideActions className={' '} tweet={tweet.parent} /> : null
 }
 
-export function Tweet(props) { // Returns list of tweets
-    const {tweet, didRetweet, hideActions} = props
+export function Tweet(props) {
+    const {tweet, didRetweet, hideActions, isRetweet, retweeter} = props
     const [actionTweet, setActionTweet] = useState(props.tweet ? props.tweet : null)
-    const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+    let className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+    className = isRetweet === true ? `${className} p-2 border rounded` : className
     const path = window.location.pathname
     const match = path.match(/(?<tweetid>\d+)/)
     const urlTweetId = match ? match.groups.tweetid : -1
@@ -34,20 +30,40 @@ export function Tweet(props) { // Returns list of tweets
         }
 
     }
-    return <div className={className}>
-        <div>
-            <p>{tweet.id} - {tweet.content}</p>
-            <ParentTweet tweet={tweet} />
-        </div>
-        <div className='btn btn-group'>
-            {(actionTweet && hideActions !== true) && <React.Fragment>
-                <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "like", display:"Likes"}}/>
-                <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "unlike", display:"Unlike"}}/>
-                <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "retweet", display:"Retweet"}}/>
-            </React.Fragment>
-            }
-            {isDetail === true ? null : <button className='btn btn-outline-primary btn-sm' onClick={handleLink}>View</button>}
-        </div>
 
+    return <div className={className}>
+        {isRetweet === true && <div className='mb-2'>
+            <span className='small text-muted'>Retweet by @{retweeter.username}</span>
+        </div>}
+        <div className='d-flex'>
+
+            <div className=''>
+            <span className='mx-1 px-3 py-2 rounded-circle bg-dark text-white'>
+            {tweet.user.username[0]}
+            </span>
+            </div>
+            <div className='col-11'>
+                <div>
+
+                    <p>
+                        {tweet.user.first_name}{" "}
+                        {tweet.user.last_name}{" "}
+                        @{tweet.user.username}
+                    </p>
+                    <p>{tweet.content}</p>
+
+                    <ParentTweet tweet={tweet} retweeter={tweet.user} />
+                </div>
+                <div className='btn btn-group px-0'>
+                    {(actionTweet && hideActions !== true) && <React.Fragment>
+                        <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "like", display:"Likes"}}/>
+                        <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "unlike", display:"Unlike"}}/>
+                        <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{type: "retweet", display:"Retweet"}}/>
+                    </React.Fragment>
+                    }
+                    {isDetail === true ? null : <button className='btn btn-outline-primary btn-sm' onClick={handleLink}>View</button>}
+                </div>
+            </div>
+        </div>
     </div>
 }
