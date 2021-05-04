8568895b582ea708c12ea2a4a8b10d9f4aeb008b
Richard Guaman <gs.richarda@gmail.com>
2021-05-04 22:25:10 +0100

    pagination in react handled

--- react-web/src/tweets-modules/detail.js
+++ react-web/src/tweets-modules/detail.js
@@ -1,11 +1,16 @@
-import React, {useState} from "react";
-import {ActionBtn} from "./buttons";
+import React, {useState}  from 'react'
+
+import {ActionBtn} from './buttons'
+
+import {
+    UserDisplay,
+    UserPicture
+} from '../profiles'
 
 export function ParentTweet(props){
     const {tweet} = props
     return tweet.parent ? <Tweet isRetweet retweeter={props.retweeter} hideActions className={' '} tweet={tweet.parent} /> : null
 }
-
 export function Tweet(props) {
     const {tweet, didRetweet, hideActions, isRetweet, retweeter} = props
     const [actionTweet, setActionTweet] = useState(props.tweet ? props.tweet : null)
@@ -33,22 +38,18 @@ export function Tweet(props) {
 
     return <div className={className}>
         {isRetweet === true && <div className='mb-2'>
-            <span className='small text-muted'>Retweet by @{retweeter.username}</span>
+            <span className='small text-muted'>Retweet via <UserDisplay user={retweeter} /></span>
         </div>}
         <div className='d-flex'>
 
             <div className=''>
-            <span className='mx-1 px-3 py-2 rounded-circle bg-dark text-white'>
-            {tweet.user.username[0]}
-            </span>
+                <UserPicture user={tweet.user} />
             </div>
             <div className='col-11'>
                 <div>
 
                     <p>
-                        {tweet.user.first_name}{" "}
-                        {tweet.user.last_name}{" "}
-                        @{tweet.user.username}
+                        <UserDisplay includeFullName user={tweet.user} />
                     </p>
                     <p>{tweet.content}</p>
 
